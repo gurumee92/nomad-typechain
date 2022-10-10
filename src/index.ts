4af7e5639897e5d4b1fb02d5fbda0e3b965d50e2
장현우 <gurumee92@gmail.com>
2022-10-10 12:01:35 +0900

    lecture clear

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,57 @@
-import { exit } from "myPackage";
+import crypto from "crypto";
 
-class Block {
-	constructor(private data: string) {}
+interface BlockShape {
+	hash: string
+	prevHash: string
+	height: number
+	data: string
+}
 
-	static hello() {
-		return "Hi"
+class Block implements BlockShape {
+	public readonly hash: string;
+	constructor(
+		public prevHash: string,
+		public height: number,
+		public data: string
+	) {
+		this.hash = Block.calculateHash(prevHash, height, data);
+	}
+
+	static calculateHash(prevHash: string, height: number, data: string): string {
+		const toHash = `${prevHash}${height}${data}`;
+		return crypto.createHash("sha256").update(toHash).digest("hex"); 
 	}
 }
 
-exit(1);
+class BlockChain {
+	private blocks: Block[]
+	constructor() {
+		this.blocks = [];
+	}
+
+	private getPreviousHash() {
+		if (this.blocks.length === 0) {
+			return "";
+		} else {
+			return this.blocks[this.blocks.length-1].hash;
+		}
+	}
+
+	public addBlock(data: string) {
+		const block = new Block(this.getPreviousHash(), this.blocks.length + 1, data);
+		this.blocks.push(block);
+	}
+
+	public getBlocks() {
+		return [...this.blocks];
+	}
+}
+
+const blockChain = new BlockChain();
+blockChain.addBlock("First");
+blockChain.addBlock("Second");
+blockChain.addBlock("Third");
+
+blockChain.getBlocks().push(new Block("XXXXX", 5151, "HACKED"));
+
+console.log(blockChain.getBlocks());
